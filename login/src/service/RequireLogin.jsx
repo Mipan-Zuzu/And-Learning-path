1ac174222105227e91a0af174381860199bca101
Mipan-Zuzu <kadekisaka00@gmail.com>
2026-01-11 14:32:23 +0800

    test: change api

--- login/src/service/RequireLogin.jsx
+++ login/src/service/RequireLogin.jsx
@@ -1,21 +1,22 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-const API_URL = "https://fixed-ant-ands-9cc7ffdd.koyeb.app";
+const PUB_API = import.meta.env.VITE_API_PUB
+const LOC_API = import.meta.env.VITE_API_LOC
 
 export default function ProtectedRoute({ children }) {
     const navigate = useNavigate()
     useEffect(() => {
         const testing = async () => {
-        const res = await fetch(`${API_URL}/check-session`,{
+        const res = await fetch(`https://fixed-ant-ands-9cc7ffdd.koyeb.app/check-session`,{
             method: "GET",
         credentials: "include"
         })
         const data = await res.json()
         if(data.login === "") {
-            navigate("/login")
+            navigate("/")
         }
-        data.login === false ? navigate("/login") : console.log({ message : "berhasil terverifikasi"})
+        data.login === false ? navigate("/") : console.log({ message : "berhasil terverifikasi"})
     }
     testing()
     })
