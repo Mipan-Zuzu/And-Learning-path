55bece977851cc6d12d8e8258a249eefe232a710
Mipan-Zuzu <kadekisaka00@gmail.com>
2026-01-13 14:28:33 +0800

    fix : url path api

--- login/src/pages/RegisterPages.jsx
+++ login/src/pages/RegisterPages.jsx
@@ -37,7 +37,7 @@ const RegisterPage = () => {
 
 
         const sendApi = () => {
-            fetch(`https://fixed-ant-ands-9cc7ffdd.koyeb.app`, {
+            fetch(`https://fixed-ant-ands-9cc7ffdd.koyeb.app/result`, {
             method : "POST",
             headers : {"Content-Type": "application/json"},
             body : JSON.stringify({ Email: Email ,Password : Password})
