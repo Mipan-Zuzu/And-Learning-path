9af3e4fd1862972699ce5be93ba6f1f00b66526f
Mipan-Zuzu <kadekisaka00@gmail.com>
2026-01-17 22:53:55 +0800

    update: using new url

--- login/src/pages/LoginPage.jsx
+++ login/src/pages/LoginPage.jsx
@@ -33,7 +33,7 @@ const LoginPage = () => {
     }
     
     const checkLogin = async () => {
-      const res = await fetch(`https://fixed-ant-ands-9cc7ffdd.koyeb.app/login`, {
+      const res = await fetch(`https://personal-talya-and-path-learning-38ee2f3f.koyeb.app/login`, {
         method: "POST",
         credentials: "include",
         headers: { "Content-Type": "application/json" },
--- login/src/pages/RegisterPages.jsx
+++ login/src/pages/RegisterPages.jsx
@@ -37,7 +37,7 @@ const RegisterPage = () => {
 
 
         const sendApi = () => {
-            fetch(`https://fixed-ant-ands-9cc7ffdd.koyeb.app/result`, {
+            fetch(`https://personal-talya-and-path-learning-38ee2f3f.koyeb.app/result`, {
             method : "POST",
             headers : {"Content-Type": "application/json"},
             body : JSON.stringify({ Email: Email ,Password : Password})
